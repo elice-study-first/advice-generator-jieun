6756521127086acf608b76cb8214495a76bb022f
jieun <balllling92@gmail.com>
2023-10-12 02:16:34 +0900

    :triangular_flag_on_post: Feat: adviceCard 컴포넌트 추가
    컴포넌트 추가 및 UI작업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './style/tailwind.css';
+import AdviceCard from './components/adviceCard';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App bg-[#1F2632] w-full h-screen flex items-center justify-center">
+      <AdviceCard />
+    </div>
   );
 }
 
---
+++ src/components/adviceCard.js
@@ -0,0 +1,9 @@
+export default function AdviceCard() {
+
+    return (
+        <section className="w-1/3 max-h-96 min-h-60 p-8 bg-[#4E5D73] rounded-lg text-center relative">
+            <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #1</h2>
+            <div className='text-lg font-semibold -tracking-tighter my-8 text-white break-keep'>테스트용 텍스트 입니다.</div>
+        </section>
+    )
+}
