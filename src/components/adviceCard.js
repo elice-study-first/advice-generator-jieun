e42c45400c692ecd6f351b917c546e667e768549
jieun <balllling92@gmail.com>
2023-10-12 11:43:47 +0900

    :triangular_flag_on_post: Feat: newAdviceButton 컴포넌트 구현
    컴포넌트 구현 및 click이벤트 작성

--- src/components/adviceCard.js
+++ src/components/adviceCard.js
@@ -1,27 +1,37 @@
 import { useState, useEffect } from "react";
+import NewAdviceButton from "./newAdviceButton";
 
 export default function AdviceCard() {
     // API 주소
     const ADVICE_API = "https://api.adviceslip.com/advice";
-
-    // 데이터 state
-    const [adviceData, setAdviceData] = useState([]);
 
     // api 데이터 가져오기
     useEffect(() => {
         fetch(ADVICE_API)
             .then(res => res.json())
             .then(data => {
-                console.log(data.slip)
+                console.log(data.slip);
                 setAdviceData(data.slip);
             })
     }, []);
 
+    // 데이터 state
+    const [adviceData, setAdviceData] = useState([]);
+    const [changeMessage, setChangeMessage] = useState(adviceData);
+
+
+    const onClickHandle = () => {
+        setChangeMessage(adviceData);
+    }
+        // const handleOnClick = useEffect(() => {
+        //     setChangeMessage(adviceData);
+        // }, [adviceData])
 
     return (
         <section className="w-1/3 max-h-96 min-h-60 p-8 bg-[#4E5D73] rounded-lg text-center relative">
-            <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{adviceData.id}</h2>
-            <div className='text-lg font-semibold -tracking-tighter my-8 text-white break-keep'>"{adviceData.advice}"</div>
+            <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{changeMessage.id}</h2>
+            <div className='text-lg font-semibold -tracking-tighter my-8 text-white break-keep'>"{changeMessage.advice}"</div>
+            <NewAdviceButton onClickHandle={onClickHandle} />
         </section>
     )
 }
---
+++ src/components/newAdviceButton.js
@@ -0,0 +1,10 @@
+import diceIcon from '../images/icon-dice.svg'
+
+export default function NewAdviceButton({onClickHandle}) {
+
+    return(
+        <div onClick={onClickHandle} className='bg-[#52FFA8] w-14 h-14 rounded-full flex items-center justify-center absolute left-2/4 -translate-x-2/4 cursor-pointer'>
+            <img src={diceIcon}/>
+        </div>
+    )
+};
