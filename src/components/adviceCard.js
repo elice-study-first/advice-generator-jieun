762018295d85ecfc7dd18f7ae535e9148137a681
jieun <balllling92@gmail.com>
2023-10-13 07:01:44 +0900

    :lipstick: Feat: divider 이미지 추가

--- src/components/adviceCard.js
+++ src/components/adviceCard.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
 import { useQuery } from "react-query";
 import NewAdviceButton from "./newAdviceButton";
+import dividerImg from "../images/pattern-divider-desktop.svg";
 
 export default function AdviceCard() {
     // API 주소
@@ -18,14 +18,15 @@ export default function AdviceCard() {
     }
 
     return (
-        <section className="w-1/3 max-h-96 min-h-60 p-8 bg-[#4E5D73] rounded-lg text-center relative">
+        <section className="w-1/3 max-h-96 min-h-60 p-8 bg-[#323A49] rounded-lg text-center relative shadow-[0_10px_20px_rgba(21,_26,_33,_0.7)]">
             {
                 isLoading ? <div className="text-white text-lg font-semibold -tracking-tighter my-8 break-keep">LOADING...</div> :
-                        <>
-                            <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{data.slip.id}</h2>
-                            <div className='text-white text-lg font-semibold -tracking-tighter my-8 break-keep'>"{data.slip.advice}"</div>
-                        </>
+                    <>
+                        <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{data.slip.id}</h2>
+                        <div className='text-white text-xl font-semibold -tracking-tighter my-8 break-keep'>"{data.slip.advice}"</div>
+                    </>
             }
+            <img className="inline-block text-center mb-8" src={dividerImg} />
             <NewAdviceButton handleClickBtn={handleClickBtn} />
         </section>
     )
