838706a7784827a924892373cbb9aa364814e90e
jieun <balllling92@gmail.com>
2023-10-13 00:38:47 +0900

    :lipstick: Feat: 버튼 hover기능 추가
    버튼 hover시 부드럽게 box-shadow되는 기능

--- src/components/adviceCard.js
+++ src/components/adviceCard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useQuery } from "react-query";
 import NewAdviceButton from "./newAdviceButton";
 
@@ -6,48 +6,25 @@ export default function AdviceCard() {
     // API 주소
     const ADVICE_API = "https://api.adviceslip.com/advice";
 
-    // api 데이터 가져오기
-    // useEffect(() => {
-    //     fetch(ADVICE_API)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data.slip);
-    //             setAdviceData(data.slip);
-    //         })
-    // }, []);
-
-    // 데이터 state
-    // const [adviceData, setAdviceData] = useState([]);
-    // const [changeMessage, setChangeMessage] = useState(adviceData);
-
-
-    // const onClickHandle = () => {
-    //     setChangeMessage(adviceData);
-    // }
-    const { isLoading, data, error, refetch } = useQuery('adviceData', async () => {
+    // react qurey로 데이터 불러오기
+    const { isLoading, data, refetch } = useQuery('adviceData', async () => {
         const res = await fetch(ADVICE_API);
         return res.json();
     })
 
-    const handleClickBtn = (e) => {
-        e.preventDefault()
+    // 클릭 이벤트
+    const handleClickBtn = () => {
         refetch();
     }
-
-    useEffect(() => {
-        console.log(isLoading, data, error);
-    }, [isLoading, data, error]);
 
     return (
         <section className="w-1/3 max-h-96 min-h-60 p-8 bg-[#4E5D73] rounded-lg text-center relative">
             {
-                isLoading ? (<div className="text-white text-lg font-semibold -tracking-tighter my-8 break-keep">LOADING...</div>) :
-                    (
+                isLoading ? <div className="text-white text-lg font-semibold -tracking-tighter my-8 break-keep">LOADING...</div> :
                         <>
                             <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{data.slip.id}</h2>
                             <div className='text-white text-lg font-semibold -tracking-tighter my-8 break-keep'>"{data.slip.advice}"</div>
                         </>
-                    )
             }
             <NewAdviceButton handleClickBtn={handleClickBtn} />
         </section>
--- src/components/newAdviceButton.js
+++ src/components/newAdviceButton.js
@@ -1,10 +1,10 @@
 import diceIcon from '../images/icon-dice.svg'
 
-export default function NewAdviceButton({handleClickBtn}) {
+export default function NewAdviceButton({ handleClickBtn }) {
 
-    return(
-        <div onClick={handleClickBtn} className='bg-[#52FFA8] w-14 h-14 rounded-full flex items-center justify-center absolute left-2/4 -translate-x-2/4 cursor-pointer'>
-            <img src={diceIcon}/>
+    return (
+        <div onClick={handleClickBtn} className='bg-[#52FFA8] w-14 h-14 rounded-full flex items-center justify-center absolute left-2/4 -translate-x-2/4 cursor-pointer transition-all hover:shadow-[rgba(82,_255,_168,_0.6)_0px_0px_16px]'>
+            <img src={diceIcon} />
         </div>
     )
 };
