a1b959012fdf1c137ca3c7103aeafd24de61ca76
jieun <balllling92@gmail.com>
2023-10-15 01:42:21 +0900

    :triangular_flag_on_post: Feat: 초기값 설정
    useEffet사용 및 초기값 전달하는 state추가하여 새로고침시 초기값 출력

--- src/components/adviceCard.js
+++ src/components/adviceCard.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 
 import NewAdviceButton from "./newAdviceButton";
@@ -20,6 +20,9 @@ export default function AdviceCard() {
 
     const [adviceId, setAdviceId] = useState(null);
     const [adviceContent, setAdviceContent] = useState(null);
+    const [initilalData, setInitialData] = useState(null);
+
+    const getAdviceData = JSON.parse(localStorage.getItem("adviceData"));
 
     // 클릭 이벤트
     const handleClickBtn = () => {
@@ -32,21 +35,28 @@ export default function AdviceCard() {
             }));
         }
 
-        const getAdviceData = JSON.parse(localStorage.getItem("adviceData"));
-
         if (getAdviceData) {
             setAdviceId(getAdviceData.id);
             setAdviceContent(getAdviceData.content);
         }
-    }
+    };
+
+    // 초기값 설정
+    useEffect(() => {
+        if(getAdviceData) {
+            setInitialData(getAdviceData);   
+        }
+
+        console.log(initilalData)
+    }, [data])
 
     return (
         <section className="w-1/3 max-h-96 min-h-60 p-8 bg-[#323A49] rounded-lg text-center relative shadow-[0_10px_20px_rgba(21,_26,_33,_0.7)]">
             {
                 isLoading ? <div className="text-white text-lg font-semibold -tracking-tighter my-8 break-keep">LOADING...</div> :
                     <>
-                        <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{adviceId}</h2>
-                        <div className='text-white text-xl font-semibold -tracking-tighter my-8 break-keep'>"{adviceContent}"</div>
+                        <h2 className="text-[#52FFA8] text-sm font-medium -tracking-tighter">ADVICE #{adviceId !== null ? adviceId : initilalData.id}</h2>
+                        <div className='text-white text-xl font-semibold -tracking-tighter my-8 break-keep'>"{adviceContent !== null ? adviceContent : initilalData.content}"</div>
                     </>
             }
             <img className="inline-block text-center mb-8" src={dividerImg} alt="구분선 이미지" />
